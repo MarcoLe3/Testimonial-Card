3733906e02217433553b606630bb68fdb80d345d
Marco Lee <marcole32k3@gmail.com>
2025-08-11 19:45:36 -0700

    styling

--- my-testimonial-card/src/components/Profile.tsx
+++ my-testimonial-card/src/components/Profile.tsx
@@ -2,11 +2,11 @@
 
 function Profile() {
   return (
-    <div className="flex flex-row gap-4 mb-4">
+    <div className="flex flex-row gap-10 mb-4">
         <img src="./Fuecoco.png" alt="" className="w-[90px] h-[100px]"/>
         <div>
-            <h2 className="text-[40px]">Marco Lee</h2>
-            <p>@Marco_Lee</p>
+            <h2 className="text-[40px] font-semibold">Marco Lee</h2>
+            <p className="font-medium">@Marco_Lee</p>
         </div>
     </div>
   )
--- my-testimonial-card/src/components/card/Card.tsx
+++ my-testimonial-card/src/components/card/Card.tsx
@@ -3,7 +3,7 @@ import TextSection from '../Paragraph'
 
 function Card() {
   return (
-    <div className='max-w-[600px] max-h-[600px] shadow-lg rounded-lg p-10 m-10'>
+    <div className='max-w-[600px] max-h-[600px] shadow-lg rounded-lg p-8 m-10 font-inter hover:cursor-pointer'>
         <Header />
         <TextSection />
     </div>
